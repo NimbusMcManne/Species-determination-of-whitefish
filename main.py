486e1804393b50e347ae363733bddfb6b0ac9c03
Lollover45 <70514379+Lollover45@users.noreply.github.com>
2025-11-06 12:21:04 +0200

    added augmentations for cropped fish images

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import os
 
 # Global variables
 non_augmented_images_path = os.path.join(os.path.dirname(__file__), "non-augmented_images")
-augmented_images_path = os.path.join(os.path.dirname(__file__), "augmented_images")
+augmented_images_path = os.path.join(os.path.dirname(__file__), "augmented_images_test")
 
 def main():
     aug = augment.ImgAugment()
 
     images = aug.load_images(non_augmented_images_path)
     # aug.example_augment()
-    aug.augmentation(images)
+    #aug.augmentation(images)
+    aug.augment(augmented_images_path, number_of_aug_images_per_img=3, images=images)
 
 
 if __name__ == "__main__":
